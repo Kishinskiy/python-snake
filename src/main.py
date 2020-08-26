7005ecc3406e2be36a7c1c7c33025d0601b8628a
Oleg Kishinskiy <oleg@kishinskiy.ru>
2020-08-26 22:35:23 +0300

    add game board

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import pygame
 
+SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
-size = [400, 600]
-
+WHITE = (255,255,255)
+BLUE =  (204,255,255)
+size = [480, 600]
+COUNT_BLOCK = 20
+MARGIN = 1
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Snake')
 
@@ -12,4 +16,16 @@ while True:
             print(exit)
             pygame.quit()
     screen.fill(FRAME_COLOR)
+
+    for row in range(COUNT_BLOCK):
+        for column in range(COUNT_BLOCK):
+            if (row + column)%2==0:
+                color = BLUE
+            else:
+                color = WHITE
+
+            pygame.draw.rect(screen, color, [10+column*SIZE_BLOCK + MARGIN*(column+1),
+                                            20+row*SIZE_BLOCK + MARGIN*(row+1),
+                                            SIZE_BLOCK, SIZE_BLOCK])
+
     pygame.display.flip()
